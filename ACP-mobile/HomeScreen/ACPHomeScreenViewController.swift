78b1efbe4ce7de053616b705884287da1babdcb9
Adi <adnan.memic98@gmail.com>
2022-09-26 19:55:15 +0100

    Init

---
+++ ACP-mobile/HomeScreen/ACPHomeScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ACPHomeScreenViewController.swift
+//  ACP-mobile
+//
+//  Created by Adi on 26/09/2022.
+//
+
+import UIKit
+
+class ACPHomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
