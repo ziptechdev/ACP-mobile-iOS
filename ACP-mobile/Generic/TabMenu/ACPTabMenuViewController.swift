8317eb7d71dcb8ce65994067cb7fc1c21b5c78cd
Adi <adnan.memic98@gmail.com>
2022-10-06 04:28:25 +0100

    Added Better Tab menu

---
+++ ACP-mobile/Generic/TabMenu/ACPTabMenuViewController.swift
@@ -0,0 +1,236 @@
+//
+//  ACPTabMenuViewController.swift
+//  ACP-mobile
+//
+//  Created by Adi on 05/10/2022.
+//
+
+import UIKit
+import SnapKit
+
+protocol ACPTabMenuViewControllerDelegate: AnyObject {
+    var numberOfItems: Int { get }
+
+    func setupViews(collectionView: UICollectionView, containerView: UIView)
+    func cellForIndex(_ collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell
+    func didSelectTab(index: Int) -> UIViewController
+}
+
+class ACPTabMenuViewController: UIViewController {
+
+	// MARK: - Properties
+
+    private let allTabsEnabled: Bool
+
+    private var currentTabItem: Int
+
+    private var childVC: UIViewController?
+
+    weak var delegate: ACPTabMenuViewControllerDelegate? {
+        didSet {
+            didSetDelegate()
+        }
+    }
+
+    // MARK: - Views
+
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let collectionView: UICollectionView = {
+        let flow = UICollectionViewFlowLayout()
+        flow.minimumInteritemSpacing = 0
+        flow.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        let view = UICollectionView(frame: .zero, collectionViewLayout: flow)
+        view.showsVerticalScrollIndicator = false
+        view.showsHorizontalScrollIndicator = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    // MARK: - Initialization
+
+    init(initialTab: Int = 0, allTabsEnabled: Bool = false) {
+        self.currentTabItem = initialTab
+        self.allTabsEnabled = allTabsEnabled
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        selectTab(index: currentTabItem)
+    }
+
+    // MARK: - UI
+
+    private func setupUI() {
+        addSubviews()
+    }
+
+    private func addSubviews() {
+        view.addSubview(containerView)
+        view.addSubview(collectionView)
+    }
+
+    // MARK: - Callback
+
+    private func didSetDelegate() {
+        delegate?.setupViews(collectionView: collectionView, containerView: containerView)
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        collectionView.reloadData()
+    }
+
+    // MARK: - Child View Controller
+
+    private func add(asChildViewController viewController: UIViewController) {
+        if let controllerToRemove = childVC {
+            remove(asChildViewController: controllerToRemove)
+        }
+
+        childVC = viewController
+
+        addChild(viewController)
+        containerView.addSubview(viewController.view)
+
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+        viewController.view.snp.makeConstraints { make in
+            make.top.left.bottom.right.equalToSuperview()
+        }
+
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        viewController.didMove(toParent: self)
+    }
+
+    private func remove(asChildViewController viewController: UIViewController) {
+        viewController.willMove(toParent: nil)
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParent()
+    }
+
+    // MARK: - Selection
+
+    func selectTab(index: Int) {
+        collectionView.selectItem(
+            at: IndexPath(item: index, section: 0),
+            animated: false,
+            scrollPosition: .centeredHorizontally
+        )
+        collectionView(collectionView, didSelectItemAt: IndexPath(item: index, section: 0))
+    }
+
+    func setupOtherCells(newIndex: IndexPath) {
+        if allTabsEnabled {
+            let previousIndexPath = IndexPath(item: currentTabItem, section: 0)
+            if let previousCell = collectionView.cellForItem(at: previousIndexPath) as? ACPFocusableView {
+                previousCell.onInactive()
+            }
+        } else {
+            let indexesToSetup: [Int] = Array(newIndex.item...currentTabItem)
+            indexesToSetup.forEach { index in
+                let indexPath = IndexPath(item: index, section: 0)
+                if let cell = collectionView.cellForItem(at: indexPath) as? ACPFocusableView {
+                    if indexPath.item <= newIndex.item {
+                        cell.onInactive()
+                    } else {
+                        cell.onDisable()
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ACPTabMenuViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return delegate?.numberOfItems ?? 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = delegate?.cellForIndex(collectionView, indexPath: indexPath) else {
+            return UICollectionViewCell()
+        }
+
+        if let focusableCell = cell as? ACPFocusableView {
+            if allTabsEnabled || (indexPath.item <= currentTabItem) {
+                focusableCell.onInactive()
+            } else {
+                focusableCell.onDisable()
+            }
+        }
+
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ACPTabMenuViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        setupOtherCells(newIndex: indexPath)
+
+        if let cell = collectionView.cellForItem(at: indexPath) as? ACPFocusableView {
+            cell.onActive()
+        }
+
+        currentTabItem = indexPath.item
+
+        guard let viewController = delegate?.didSelectTab(index: currentTabItem) else {
+            return
+        }
+
+        add(asChildViewController: viewController)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        if allTabsEnabled {
+            return true
+        } else {
+            return indexPath.item <= currentTabItem
+        }
+    }
+}
+
+extension ACPTabMenuViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        var contentWidthInset = collectionView.contentInset.left + collectionView.contentInset.right
+        let numberOfItems = CGFloat(delegate?.numberOfItems ?? 0)
+        // Add inter item spacing so the cells can be at maximum width
+        contentWidthInset += numberOfItems
+        let cellWidth = (collectionView.frame.width - contentWidthInset) / numberOfItems
+
+        let contentHeightInset = collectionView.contentInset.top + collectionView.contentInset.bottom
+        let cellHeight = collectionView.frame.height - contentHeightInset
+
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+}
