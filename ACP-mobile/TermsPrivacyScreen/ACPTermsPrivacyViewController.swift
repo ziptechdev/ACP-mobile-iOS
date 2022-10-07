321d95ee7b03fac6619b5f7e8bb365c8ffb9ce6a
Eldar Tutnjic <eldartutnjic@gmail.com>
2022-10-07 05:50:12 +0200

    terms&privacy refactor

---
+++ ACP-mobile/TermsPrivacyScreen/ACPTermsPrivacyViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ACPTermsPrivacyViewController.swift
+//  ACP-mobile
+//
+//  Created by Eldar Tutnjic on 7. 10. 2022..
+//
+
+import Foundation
---
+++ ACP-mobile/TermsPrivacyScreen/ACPWelcomeScreeViewControllern.swift
@@ -0,0 +1,8 @@
+//
+//  ACPWelcomeScreeViewControllern.swift
+//  ACP-mobile
+//
+//  Created by Eldar Tutnjic on 7. 10. 2022..
+//
+
+import Foundation
