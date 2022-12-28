27042aedb6d069603585bf8bea4af3c17a460d68
Abi <abdulah.dzafic@gmail.com>
2022-12-29 00:49:25 +0100

    Update AppCoordinator.swift
    Update first screen to Welcome.

--- ACP-mobile/App/AppCoordinator.swift
+++ ACP-mobile/App/AppCoordinator.swift
@@ -40,7 +40,7 @@ class AppCoordinator: Coordinator {
     func goToWelcome() {
         UserDefaults.standard.set(true, forKey: Constants.initialLaunch)
 
-        let coordinator = LoginCoordinator(navigationController: navigationController)
+        let coordinator = WelcomeCoordinator(navigationController: navigationController)
         addChild(coordinator)
     }
 
